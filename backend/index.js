6009975e38d1c3675ae129b8643ab952ff426b43
WojciechTechstarter <wojciech.olakowski@tn.techstarter.de>
2025-05-02 07:38:20 +0200

    addded 3 countries to the database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,11 @@
 const express = require("express")
 const app = express()
+// Importing the sqlite3 module to work with SQLite databases
+const sqlite3 = require("sqlite3").verbose()
+// db is the database connection object
+const db = new sqlite3.Database("./database/traveljournal.db")
+// Importing the cors module to enable Cross-Origin Resource Sharing
+
 
 //Middleware to parse JSON data from incoming requests
 app.use(express.json())
@@ -8,6 +14,31 @@ app.use(express.json())
 app.get("/", (req, res) => {
     res.send("MemoryMiles backend is running!")
 })
+
+// db.run(`INSERT INTO countries (name, continent_id) VALUES ("Portugal", 1)`);
+// db.run(`INSERT INTO countries (name, continent_id) VALUES ("Japan", 2)`);
+// db.run(`INSERT INTO countries (name, continent_id) VALUES ("Cyprus", 1)`);
+
+
+
+
+
+
+app.get("/countries", (req, res) => {
+    // Query to select all countries from the database
+    db.all(`SELECT * FROM countries`, [], (err, rows) => {
+        if (err) {
+            console.error(err)
+            res.status(500).send("Internal server error")
+        } else {
+            res.status(200).json(rows)
+        }
+    })
+})
+
+
+
+
 
 app.listen(5005, () => {
     console.log("The server is running on http://localhost:5005")
--- backend/init.sql
+++ backend/init.sql
@@ -10,6 +10,13 @@ CREATE TABLE IF NOT EXISTS countries (
     FOREIGN KEY (continent_id) REFERENCES continents(id)
 );
 
+CREATE TABLE IF NOT EXISTS countries (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    continent_id INTEGER NOT NULL,
+    FOREIGN KEY (continent_id) REFERENCES continents(id)
+);
+
 CREATE TABLE IF NOT EXISTS places (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
